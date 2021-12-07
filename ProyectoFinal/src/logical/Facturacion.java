097c74800d1166272213e659c2d04787257dbc25
WINDOWS <WINDOWS@DESKTOP-4VKQUKM>
2021-12-07 01:22:57 -0400

    facturacion logical edit

--- ProyectoFinal/src/logical/Facturacion.java
+++ ProyectoFinal/src/logical/Facturacion.java
@@ -14,6 +14,7 @@ public class Facturacion {
 	private String tipopago;
 	private boolean pagado;
 	private Object[][]filas;
+	private int cant;
 	public Facturacion(String codigo, float precioTotal, Usuario vendedor, 
 			 Cliente cliente, boolean pagado) {
 		super();
@@ -26,7 +27,52 @@ public class Facturacion {
 		this.tipopago = tipopago;
 		this.pagado = pagado;
 		this.filas = new Object [100][5];
+		this.cant=-1;
 	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public String getTipopago() {
+		return tipopago;
+	}
+	public void setTipopago(String tipopago) {
+		this.tipopago = tipopago;
+	}
+	public float getPrecioTotal() {
+		return precioTotal;
+	}
+	public void setPrecioTotal(float precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	public Usuario getVendedor() {
+		return vendedor;
+	}
+	public void setVendedor(Usuario vendedor) {
+		this.vendedor = vendedor;
+	}
+	public Object[][] getFilas() {
+		return filas;
+	}
+	public void setFilas(Object[][] filas) {
+		this.filas = filas;
+	}
+	public int getCant() {
+		return cant;
+	}
+	public void setCant(int cant) {
+		this.cant = cant;
+	}
+
+	
 	public void agregarfila(int pos,Object[] o) {
 		for(int i = 0; i<5;i++) {
 			filas[pos][i]=o[i];
@@ -39,5 +85,9 @@ public class Facturacion {
 	public void agregarcomponente(Componente c) {
 		miscomponentes.add(c);
 	}
+
+
+	
+	
 
 }
